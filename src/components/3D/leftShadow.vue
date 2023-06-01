d548e62a4ed7f525cb109c3be3b52fdd36fda109
安学玉 <1358042645@qq.com>
2023-06-01 16:38:59 +0800

    3D卡片文字等等

---
+++ src/components/3D/leftShadow.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="leftShadow">{{text}}</div>
+</template>
+<script>
+import { defineComponent, reactive, toRefs } from 'vue';
+export default defineComponent({
+	name: 'text',
+	props: {
+        text:{
+            type:String,
+            default:'left Txt Shadow'
+        }
+    },
+	setup() {
+		const data = reactive({
+		});
+		return {
+			...toRefs(data),
+		};
+	},
+});
+</script>
+<style lang="scss" scoped>
+@function makelongleftshadow($color) {
+    $val: 0px 0px $color;
+    @for $i from 1 through 25 {
+        $color: fade-out(desaturate($color, 1%), .06);
+        $val: #{$val}, -#{$i}px #{$i}px #{$color};
+    }
+    @return $val;
+}
+.leftShadow{
+	text-align: center;
+    font-size: 40px;
+    line-height: 60px;
+	text-shadow: makelongleftshadow(rgba(174, 14, 214, 0.548));
+    color: hsl(310, 90%, 61%);
+}
+</style>
---
+++ src/components/3D/rightShadow.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="rightShadow">{{text}}</div>
+</template>
+<script>
+import { defineComponent, reactive, toRefs } from 'vue';
+export default defineComponent({
+	name: 'text',
+	props: {
+        text:{
+            type:String,
+            default:'Right Txt Shadow'
+        }
+    },
+	setup() {
+		const data = reactive({
+		});
+		return {
+			...toRefs(data),
+		};
+	},
+});
+</script>
+<style lang="scss" scoped>
+@function makelongrightshadow($color) {
+    $val: 0px 0px $color;
+    @for $i from 1 through 10 {
+        $color: fade-out(desaturate($color, 1%), .1);
+        $val: #{$val}, #{$i}px #{$i}px #{$color};
+    }
+    @return $val;
+}
+.rightShadow {
+    text-align: center;
+    font-size: 40px;
+    line-height: 60px;
+    text-shadow: makelongrightshadow(hsla(182, 100%, 30%, 1));
+    color: hsl(182, 100%, 60%);
+}
+</style>
