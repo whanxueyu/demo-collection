bc19b33b56ef810a3e0e1d8d228b66d900755a8e
175693420127 <1358042645@qq.com>
2024-08-10 15:18:40 +0800

    style： 修改图标

--- src/static/baseMap/index.js
+++ src/static/baseMap/index.js
@@ -1,12 +1,16 @@
 import tdt_img from './tdt_img.png'
 import tdt_vec from './tdt_vec.png'
 import gaode_img from './gaode_img.png'
-import gaode_vec from './gaode_vec.png'
+import gaode_vec from './gaode_vec.jpg'
+import bing_img from './bing_img.jpg'
+import bing_vec from './bing_vec.jpg'
 const baseMapIcon = {
     tdt_img: tdt_img,
     tdt_vec: tdt_vec,
     gaode_img: gaode_img,
     gaode_vec: gaode_vec,
+    bing_img: bing_img,
+    bing_vec: bing_vec,
 }
 
 export default baseMapIcon
